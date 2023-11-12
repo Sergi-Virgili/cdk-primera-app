52f21f6b4594521d223cb9180bebadd5cb4adc9c
Sergi-Virgili <svirgilif@gmail.com>
2023-11-12 10:53:39 +0100

    Initial commit

---
+++ test/cdk-primera-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPrimeraApp from '../lib/cdk-primera-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-primera-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPrimeraApp.CdkPrimeraAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
